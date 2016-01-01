8b6e2a1996730759359301b062910d123963e8cb
mcli <mclimber28@gmail.com>
2016-01-01 08:52:36 -0400

    año nuevo estaticos

--- Login/settings.py
+++ Login/settings.py
@@ -18,7 +18,8 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.dirname(__file__)
+PUBLIC_DIR = os.path.join(PROJECT_DIR, 'public')
 
 
 # Quick-start development settings - unsuitable for production
@@ -61,13 +62,30 @@ ROOT_URLCONF = 'Login.urls'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = os.path.join(PUBLIC_DIR, 'static')
+
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
+# Additional locations of static files
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, 'static'),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'static'),
 )
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+# 	os.path.join(BASE_DIR, 'static'),
+# )
 
 print STATICFILES_DIRS	
 TEMPLATES = [
