ed5cd098680b1b6def7680ccab7a5d46c9ba1d1a
mcli <mclimber28@gmail.com>
2015-12-31 20:24:37 -0400

    sigo con estaticos 5

--- Login/settings.py
+++ Login/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'Login.urls'
 #)
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = 'app/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static'),
