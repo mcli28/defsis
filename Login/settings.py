d3592ec4405cde6720a86a75e68df2d6731a6a35
mcli <mclimber28@gmail.com>
2016-01-01 09:04:41 -0400

    estaticos año nuevo

--- Login/settings.py
+++ Login/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'Login.urls'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
