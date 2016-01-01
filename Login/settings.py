b538d5dff6eecf6c2d48bfbe0d3f2fe6db15b24e
mcli <mclimber28@gmail.com>
2015-12-31 22:04:25 -0400

    sigo con estaticos 12

--- Login/settings.py
+++ Login/settings.py
@@ -18,8 +18,7 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-print BASE_DIR
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -63,20 +62,13 @@ ROOT_URLCONF = 'Login.urls'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static'),
 )
 
-# STATIC_ROOT = 'staticfiles'
-# STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = (
-# 	os.path.join(BASE_DIR, 'static'),
-# )
 print STATICFILES_DIRS	
 TEMPLATES = [
 	{
