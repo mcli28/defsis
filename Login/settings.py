bb23ed48efd0fcd38b0ae30c7250827f52eac502
mcli <mclimber28@gmail.com>
2015-12-31 20:48:24 -0400

    sigo con estaticos 8

--- Login/settings.py
+++ Login/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'Login.urls'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
