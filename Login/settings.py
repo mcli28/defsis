9b4ca0900aad543da178188b1564bf26629e0fd0
mcli <mclimber28@gmail.com>
2015-12-31 20:21:42 -0400

    sigo con estaticos 4

--- Login/settings.py
+++ Login/settings.py
@@ -17,8 +17,8 @@ import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 print BASE_DIR
 
 
@@ -76,12 +76,12 @@ ROOT_URLCONF = 'Login.urls'
 #)
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = 'app/static/'
 
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static'),
 )
-
+print STATICFILES_DIRS	
 TEMPLATES = [
 	{
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
