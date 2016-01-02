2a48b4dc5ff302d13478260be4ce7968c76b2b77
mcli <mclimber28@gmail.com>
2016-01-01 22:50:12 -0400

    edit estaticos

--- Login/settings.py
+++ Login/settings.py
@@ -17,7 +17,8 @@ import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print BASE_DIR+"jajaj"
 
 
@@ -77,7 +78,7 @@ print STATIC_URL
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, 'static'),
+	os.path.join(BASE_DIR, 'Login/static'),
 )
 
 print STATICFILES_DIRS	
