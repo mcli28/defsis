aaa736a5575fceb1425ab26645a62132b677b1f9
mcli <mclimber28@gmail.com>
2016-01-01 22:34:00 -0400

    estaticos 33

--- Login/settings.py
+++ Login/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 print BASE_DIR+"jajaj"
 
 
