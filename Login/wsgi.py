dcc9bcae0ee05b744db425333be504467fd556fd
mcli <mclimber28@gmail.com>
2016-01-01 23:53:03 -0400

    statics edit file wsgi 2

--- Login/wsgi.py
+++ Login/wsgi.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 # from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Login.settings")
-
 # application = get_wsgi_application()
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Login.settings")
 application = Cling(get_wsgi_application())
