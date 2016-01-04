f33b3981ab738a8ba6df2208ef0ab4c76c5c3939
mcli <mclimber28@gmail.com>
2016-01-04 07:35:16 -0400

    antes de subir

--- Login/settings.py
+++ Login/settings.py
@@ -157,4 +157,3 @@ STATICFILES_DIRS = (
 )
 
 print STATICFILES_DIRS
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- Login/wsgi.py
+++ Login/wsgi.py
@@ -7,17 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-# import os
-# # from django.core.wsgi import get_wsgi_application
+import os
 # from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Login.settings")
-# application = get_wsgi_application()
-# # application = Cling(get_wsgi_application())
-
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Login.settings")
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = Cling(get_wsgi_application())
